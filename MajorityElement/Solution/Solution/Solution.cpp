89e6eff6c7771f51c17a6de2b2af7dcce42c7ce5
NikOsadchiy <nikolas.osadchey@gmail.com>
2024-05-28 18:26:46 +0400

    Uploaded solution for Majority Element assignment

---
+++ MajorityElement/Solution/Solution/Solution.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int max, count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                max = num;
+                ++count;
+            }
+            else if (max == num) ++count;
+            else --count;
+        }
+        count = 0;
+        for (int num : nums) {
+            if (num == max) ++count;
+        }
+        if (count > nums.size() / 2) return max;
+        return -1;
+    }
+};
