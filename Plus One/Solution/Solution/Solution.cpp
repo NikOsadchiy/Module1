f8bcdfa7ac46a181a31d2ddbb00151e3822c1f09
NikOsadchiy <nikolas.osadchey@gmail.com>
2024-05-28 15:23:07 +0400

    Uploaded solution for Plus One assignment

---
+++ Plus One/Solution/Solution/Solution.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int size = digits.size();
+        int number = 1;
+
+        for (int i = size - 1; i >= 0; --i) {
+            digits[i] += number;
+            if (digits[i] == 10) {
+                digits[i] = 0;
+                number = 1;
+            }
+            else {
+                number = 0;
+            }
+        }
+
+        if (number == 1) {
+            digits.insert(digits.begin(), 1);
+        }
+
+        return digits;
+    }
+};
