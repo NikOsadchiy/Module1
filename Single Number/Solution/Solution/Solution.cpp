386f188142cd3119d5126d5231f2a217bc753bf5
NikOsadchiy <nikolas.osadchey@gmail.com>
2024-05-28 18:14:30 +0400

    Uploaded solution for Single Number assignment

---
+++ Single Number/Solution/Solution/Solution.cpp
@@ -0,0 +1,10 @@
+﻿class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+};
