54f4fcf05b8a289a1d4bd13067896d81487c5ecc
NikOsadchiy <nikolas.osadchey@gmail.com>
2024-05-29 21:40:55 +0400

    Uploaded solution for Valid Mountain Array assignment

---
+++ Valid Mountain Array/Solution/Solution/Solution.cpp
@@ -0,0 +1,11 @@
+﻿class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int i = 0;
+        while (i < n - 1 and arr[i] < arr[i + 1]) i++;
+        if (i == 0 or i == n - 1) return false;
+        while (i<n - 1 and arr[i]>arr[i + 1]) i++;
+        return i == n - 1;
+    }
+};
